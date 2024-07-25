bb2f3a97949daf96d46535707d36045282dd6e86
mar24 <chandanahm328@gmail.com>
2024-07-25 08:13:42 +0530

    1st version of quiz app

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+export const Data=[
+    {
+        question:'5+10',
+        option1: '5',
+        option2: '10',
+        option3: '15',
+        option4: '25',
+        ans : '3'
+    },
+{
+        question:'2*20',
+        option1: '20',
+        option2: '40',
+        option3: '60',
+        option4: '80',
+        ans : '2'
+},
+    {
+        question:'3+89',
+        option1: '20',
+        option2: '92',
+        option3: '30',
+        option4: '80',
+        ans : '2'
+    },
+    {
+        question:'89-9',
+        option1: '20',
+        option2: '92',
+        option3: '30',
+        option4: '80',
+        ans : '4'
+    },
+    {
+        question:'10/5',
+        option1: '3',
+        option2: '4',
+        option3: '5',
+        option4: '2',
+        ans : '4'
+    }
+
+
+]
